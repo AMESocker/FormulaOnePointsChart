2ae7e52867d00dcdc68558570e872740397c37bf
Aaron Socher <aesocher@yahoo.com>
2025-09-18 21:03:19 -0700

    added team colors

---
+++ colors.js
@@ -0,0 +1,27 @@
+// File: colors.js
+/*
+export const teamColors = {
+  "Red Bull": "#1E5BC6",
+  "Ferrari": "#E8002D",
+  "Mercedes": "#00D2BE",
+  "McLaren": "#FF8000",
+  "Aston Martin": "#006F62",
+  "Alpine F1 Team": "#0090FF",
+  "Williams": "#005AFF",
+  "RB F1 Team": "#6692FF", // Visa Cash App RB
+  "Sauber": "#00E701",
+  "Haas F1 Team": "#B6BABD",
+};
+*/
+export const teamColors = {
+'Mercedes'	:'#00D7B6',
+'Red Bull' 	:'#0e346eff',
+'Ferrari'	:'#ED1131',
+'McLaren':	'#F47600',
+'Alpine F1 Team'	:'#00A1E8',
+'RB F1 Team':	'#96b5fcff',
+'Aston Martin'	:'#014f46ff',
+'Williams':	'#1459b9ff',
+'Sauber'	:'#00E701',
+'Haas F1 Team'	:'#9C9FA2'
+};
